e2aaf42400af423d9669b2c4f52520983ef70236
simofane <simofane4@gmail.com>
2023-11-03 12:16:24 +0100

    doctor models

--- src/app/admin/doctor/doctors/doctors.model.ts
+++ src/app/admin/doctor/doctors/doctors.model.ts
@@ -1,12 +1,68 @@
 import { formatDate } from "@angular/common";
+{
+  "id": 1,
+  "user": {
+      "id": 2,
+      "email": "apalmer@palmer.com",
+      "password": "pbkdf2_sha256$320000$NSJruAkhXEVZWYr9BrwRE4$91T00UwyrwPRoAn7n1WeoPxJbsTs42uBg9/iE1Kx2MU=",
+      "last_login": null,
+      "is_superuser": false,
+      "username": "apalmer",
+      "first_name": "Amanda",
+      "last_name": "Palmer",
+      "is_staff": false,
+      "is_active": true,
+      "date_joined": "2023-10-29T12:47:01.981070Z",
+      "role": "doctor",
+      "groups": [],
+      "user_permissions": []
+  },
+  "img": "/media/doctor/default.png",
+  "inp": "82444291",
+  "gender": "Female",
+  "phone": "001-662-810-4269",
+  "address": "3851 York Key Suite 334\nOrtegafurt, AS 50894",
+  "cabinet": 1,
+  "specialiste": 38
+}
+
+
+export class User {
+
+  id: number;
+  email: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  role: string;
+
+  constructor(user){{
+    this.id = user.id;
+    this.email = user.email;
+    this.username = user.username;
+    this.first_name = user.first_name;
+    this.last_name = user.last_name;
+    this.role = user.role;
+
+  }}
+  
+}
+
+
+
+
+
+
+
 export class Doctors {
   id: number;
+  user: User;
   img: string;
-  name: string;
-  email: string;
+  inp: string;
+  gender: string;
   date: string;
   specialization: string;
-  mobile: string;
+  phone: string;
   department: string;
   degree: string;
 
@@ -16,11 +72,11 @@ export class Doctors {
     {
       this.id = doctors.id || this.getRandomID();
       this.img = doctors.avatar || "assets/images/user/user1.jpg";
-      this.name = doctors.name || "";
-      this.email = doctors.email || "";
+      this.inp = doctors.name || "";
+      this.gender = doctors.email || "";
       this.date = formatDate(new Date(), "yyyy-MM-dd", "en") || "";
       this.specialization = doctors.specialization || "";
-      this.mobile = doctors.mobile || "";
+      this.phone = doctors.mobile || "";
       this.department = doctors.department || "";
       this.degree = doctors.degree || "";
     }
